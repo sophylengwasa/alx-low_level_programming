f8b61dc18e5746a7c59b72765b916dd22c510f0d
sophylengwasa <sophylengwasa@gmail.com>
2023-03-20 08:03:06 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print Programming is like building a multilingual puzzle,using puts function
+ * Return:0 (sucess)
+ */
+int main(void)
+{
+puts("\ "Programming is like building a multilingual puzzle");
+return(0);
+}
